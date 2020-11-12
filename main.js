be666e18f2df6a791bd765545a78ceb835697f76
Senseyka <senseyikaaa@gmail.com>
2020-11-12 20:44:03 +0200

    Create Photo Redactor

---
+++ main.js
@@ -0,0 +1,61 @@
+const grayscale = document.querySelector("#grayscale")
+const contrast = document.querySelector("#contrast")
+const brightness = document.querySelector("#brightness")
+const sepia = document.querySelector("#sepia")
+const saturate = document.querySelector("#saturate")
+const reset = document.querySelector("#reset")
+const img = document.querySelector("#image")
+const imgUrl = document.querySelector("#img-url")
+
+
+let defaults = {
+   grayscale: grayscale.value,
+   contrast: contrast.value,
+   brightness: brightness.value,
+   sepia: sepia.value,
+   saturate: saturate.value,
+}
+
+grayscale.addEventListener("input", updateFilterValue)
+contrast.addEventListener("input", updateFilterValue)
+brightness.addEventListener("input", updateFilterValue)
+sepia.addEventListener("input", updateFilterValue)
+saturate.addEventListener("input", updateFilterValue)
+
+reset.addEventListener("click", resetFilterValue)
+
+imgUrl.addEventListener("change", updateImageUrl)
+
+
+function updateFilterValue() {
+   reset.disabled = false
+   img.style.filter = `
+      grayscale(${grayscale.value}%)
+      contrast(${contrast.value}%)
+      brightness(${brightness.value}%)
+      sepia(${sepia.value}%)
+      saturate(${saturate.value}%)
+   `
+}
+
+function resetFilterValue() {
+   img.style.filter = `
+      grayscale(${defaults.grayscale}%)
+      contrast(${defaults.contrast}%)
+      brightness(${defaults.brightness}%)
+      sepia(${defaults.sepia}%)
+      saturate(${defaults.saturate}%)
+   `
+
+   grayscale.value = defaults.grayscale
+   contrast.value = defaults.contrast
+   brightness.value = defaults.brightness
+   sepia.value = defaults.sepia
+   saturate.value = defaults.saturate
+
+   reset.disabled = true
+}
+
+function updateImageUrl() {
+   img.setAttribute("src", imgUrl.value)
+}
